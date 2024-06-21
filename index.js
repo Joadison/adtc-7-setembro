b36a9e91ff8f50a871c4b00e0f9b2d33f72dfc26
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-06-21 11:58:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Servidor está rodando em http://localhost:${port}`);
+});
