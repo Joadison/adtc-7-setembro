a41427edcb960d065f941c22fd1d612e255d6940
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-06-24 14:49:01 -0300

    Feat: Criado paginas e gerenciadas.

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/escala', (req, res) => {
+    res.sendFile(__dirname + '/public/escala.html');
+});
+
+app.get('/louvor', (req, res) => {
+    res.sendFile(__dirname + '/public/louvor.html');
+});
+
+
 app.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
 });
