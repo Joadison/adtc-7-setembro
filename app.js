e2e05cbca61f86d1b4832548cc3d0bdc2891a68c
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-06-26 16:09:24 -0300

    Feat: updade front

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
-const generateImageRoutes = require('./routes/generateImage');
-const generateImageARoutes = require('./routes/generateImageA');
-const { initializeBot } = require('./services/telegramBot');
-
 const app = express();
 const port = 3000;
 
@@ -32,10 +27,7 @@ app.get('/create-story', (req, res) => {
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.use('/', generateImageRoutes);
-app.use('/', generateImageARoutes);
 
 app.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
-    initializeBot();
 });
