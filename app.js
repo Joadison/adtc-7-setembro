f0bfb95811389bd8d8e6fd12b8325e61d2f71095
Joadison de Macedo Silva <F220910005@LLKNCSCCS5171.lanlink.com.br>
2024-06-26 11:46:56 -0300

    feat: Updade dados

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const generateImageRoutes = require('./routes/generateImage');
+const generateImageARoutes = require('./routes/generateImageA');
+const { initializeBot } = require('./services/telegramBot');
+
 const app = express();
 const port = 3000;
 
-const indexRoutes = require('./routes/index');
-const generateImageRoutes = require('./routes/generateImageA');
-const generateImageARoutes = require('./routes/generateImageA');
-const { initializeBot } = require('./services/telegramBot');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/escala', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'escala.html'));
+});
+
+app.get('/louvor', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'louvor.html'));
+});
+
+app.get('/create-story', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'create_story.html'));
+});
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.use('/', indexRoutes);
 app.use('/', generateImageRoutes);
 app.use('/', generateImageARoutes);
 
